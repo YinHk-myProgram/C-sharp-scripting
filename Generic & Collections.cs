7ea197e004a02e0921804a14d54738231b29e6ac
Ken <67538811+YinHk@users.noreply.github.com>
2024-01-19 06:53:48 -0500

    Add files via upload

---
+++ Generic & Collections.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Generic_and_collections
+{
+	class Script
+	{
+		//generic collection - List
+		List<string> JapaneseGirls = new List<string>() { "Erika", "Rinka", "Momoko", "Jun", "Saya", "Hikari", "Ai" };
+
+
+		static void Main(string[] args)
+		{
+			Script s = new Script();
+
+			//List
+
+		}
+	}
+}
