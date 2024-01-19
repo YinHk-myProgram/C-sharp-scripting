52b950c856b5931c2ad915d9c812f84f2c0ade72
YinHk <ministudiowingyin@gmail.com>
2024-01-19 07:18:34 -0500

    del readme

--- Generic & Collections.cs
+++ Generic & Collections.cs
@@ -7,14 +7,23 @@ namespace Generic_and_collections
 	class Script
 	{
 		//generic collection - List
-		List<string> JapaneseGirls = new List<string>() { "Erika", "Rinka", "Momoko", "Jun", "Saya", "Hikari", "Ai" };
+		List<string> list = new List<string>() { "Erika", "Rinka", "Momoko", "Jun", "Saya", "Hikari", "Ai" };
 
 
 		static void Main(string[] args)
 		{
 			Script s = new Script();
 
-			//List
+			//List<T> properties
+			int count = s.list.Count;
+			Console.WriteLine(count);
+
+			int cap = s.list.Capacity;
+			Console.WriteLine(cap);
+
+
+
+
 
 		}
 	}
